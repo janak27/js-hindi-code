ae7489149623884d6f12f321d2c3b860ec677667
JANAK BABAN AVHAD <80969920+janak27@users.noreply.github.com>
2023-10-11 20:50:21 +0000

    brhing

--- 01/test.js
+++ 01/test.js
@@ -1,1 +1,13 @@
-console.log("Hi janak");
+// console.log("Hi janak");
+
+const accountId = 44;
+let accountEmail = "janak@gmail.com";
+var accounPassword = "password@123";
+accountCity = "Mumbai";
+accountEmail = "hulahul.com"
+let accountstate ;
+
+// console.log(accountId);
+// console.log(accountEmail);
+
+console.table([accountId,accounPassword,accountstate,accountEmail,accountCity]);
