ec3c454c21f1a0f5c4540ea3da6969fd4574286a
JANAK BABAN AVHAD <80969920+janak27@users.noreply.github.com>
2024-01-20 10:30:40 +0000

    I am back

--- 01/test.js
+++ 01/test.js
@@ -1,15 +1,10 @@
-
-
 console.log("Hi janak");
-
 const accountId = 44;
 let accountEmail = "janak@gmail.com";
 var accounPassword = "password@123";
 accountCity = "Mumbai";
 accountEmail = "hulahul.com"
-let accountstate ;
-
+let accountstate  = "maharashtra";
 // console.log(accountId);
 // console.log(accountEmail);
-
 console.table([accountId,accounPassword,accountstate,accountEmail,accountCity]);
